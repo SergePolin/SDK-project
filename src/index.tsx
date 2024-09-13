ed4a73f043b82a03eff872849141c3d6af829d80
Sergey Polin <s.polin@innopolis.university>
2024-09-14 00:58:22 +0300

    update

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-  
-import App from './app';
-  
-export default () => <App/>;
-  
-let rootElement: ReactDOM.Root
-  
-export const mount = (Сomponent, element = document.getElementById('app')) => {
-  const rootElement = ReactDOM.createRoot(element);
-  rootElement.render(<Сomponent/>);
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-  if(module.hot) {
-      module.hot.accept('./app', ()=> {
-        rootElement.render(<Сomponent/>);
-      })
+import App from "./app";
+
+export default () => <App />;
+
+let rootElement: ReactDOM.Root;
+
+export const mount = (Сomponent, element = document.getElementById("app")) => {
+  const rootElement = ReactDOM.createRoot(element);
+  rootElement.render(<Сomponent />);
+
+  if (module.hot) {
+    module.hot.accept("./app", () => {
+      rootElement.render(<Сomponent />);
+    });
   }
 };
 
