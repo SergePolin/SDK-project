257d8acf25b545ca5d6e3a9497d1a44441d56786
Sergey Polin <s.polin@innopolis.university>
2024-09-28 12:19:07 +0300

    fixed url base

--- src/app.tsx
+++ src/app.tsx
@@ -24,7 +24,7 @@ const App: React.FC = () => {
               className={({ isActive }) =>
                 isActive ? "nav-element active" : "nav-element"
               }
-              to="/"
+              to="/sdk"
             >
               Dashboard
             </NavLink>
